605e167e2ee4cb4384c70bf7f3b33c4e5912bbf1
Slava Sakhnenko <slava@Viatcheslavs-MacBook-Pro.local>
2018-01-20 14:09:07 -0200

    contentful connect

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const htmlStandards = require('reshape-standard')
 const cssStandards = require('spike-css-standards')
 const jsStandards = require('spike-js-standards')
-const pageId = require('spike-page-id')
 const sugarml = require('sugarml')
 const sugarss = require('sugarss')
 const env = process.env.SPIKE_ENV
+const Contentful = require('spike-contentful')
+const standard = require('reshape-standard')
+
+const locals = {}
+
+module.exports = {
+  reshape: standard({ locals: () => locals }),
+  
+}
+
+const newLocal = 'data.json';
 
 module.exports = {
   devtool: 'source-map',
@@ -12,7 +22,7 @@ module.exports = {
   ignore: ['**/layout.sgr', '**/_*', '**/.*', 'readme.md', 'yarn.lock', 'package-lock.json'],
   reshape: htmlStandards({
     parser: sugarml,
-    locals: (ctx) => { return { pageId: pageId(ctx), foo: 'bar' } },
+    locals: (ctx) => locals,
     minify: env === 'production'
   }),
   postcss: cssStandards({
@@ -20,5 +30,26 @@ module.exports = {
     minify: env === 'production',
     warnForDuplicates: env !== 'production'
   }),
-  babel: jsStandards()
+  babel: jsStandards(),
+  plugins: [
+    new Contentful({
+      addDataTo: locals,
+      accessToken: '480640bb178b9eb1632c2730e0b37dfd46aee289385c8852592e63a779470a07',
+      spaceId: '543uoj65nye4',
+      contentTypes: [
+        {
+          name: 'cities',
+          id: 'city'
+        },
+        {
+          name: 'museums',
+          id: 'museum'
+        },
+        {
+          name: 'pieces',
+          id: 'piece'
+        }
+      ]
+    })
+]
 }
