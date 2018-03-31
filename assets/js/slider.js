7f8b7a16a9361074b285ee522f20a1c017dc8fb7
Slava Sakhnenko <slava@Viatcheslavs-MacBook-Pro.local>
2018-03-30 22:20:02 -0300

    responsive fixes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -4,23 +4,20 @@ import {tns} from "tiny-slider/src/tiny-slider.module.js"
 if (document.getElementById("features")!=null){
     var features = tns({
         container: '.featuresSlider',
-        items: 3,
+        items: 1,
         slideBy: 'page',
-        autoplay: true,
-        speed: 6000,
+        autoplay: false,
+        speed: 400,
         slideBy: 1,
         nav: false,
         controls: false,
         autoplayButtonOutput: false,
         mouseDrag: true,
         responsive: {
-            500: {
-              items: 1
-            },
-            700: {
+            600: {
               items: 2
             },
-            900: {
+            1000: {
               items: 3
             }
           }
