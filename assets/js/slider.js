26093ca9db98ed4b904827fc72b3b3f86be2b2dc
slava <slavas@gmail.com>
2018-08-24 22:14:53 -0300

    Image compression fixes

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,38 +1,37 @@
 import {tns} from "tiny-slider/src/tiny-slider.js"
 
-
 if (document.getElementById("features")!=null){
-    var features = tns({
-        container: '.featuresSlider',
-        items: 1,
-        gutter: 6,
-        slideBy: 'page',
-        autoplay: true,
-        speed: 1000,
-        autoplayTimeout: 6000,
-        slideBy: 1,
-        nav: false,
-        controls: false,
-        autoplayButtonOutput: false,
-        mouseDrag: true,
-        lazyload: false,
-        responsive: {
-            600: {
-              items: 2,
-              gutter: 6
-            },
-            1000: {
-              items: 3,
-              gutter: 6
-            },
-            1500: {
-              items: 4,
-              gutter: 6
-            },
-            2000: {
-              items: 5,
-              gutter: 6
-            }
-          }
-      });
+  var features = tns({
+    container: '.featuresSlider',
+    items: 1,
+    gutter: 6,
+    slideBy: 'page',
+    autoplay: true,
+    speed: 1000,
+    autoplayTimeout: 6000,
+    slideBy: 1,
+    nav: false,
+    controls: false,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    lazyload: false,
+    responsive: {
+        600: {
+          items: 2,
+          gutter: 6
+        },
+        1000: {
+          items: 3,
+          gutter: 6
+        },
+        1500: {
+          items: 4,
+          gutter: 6
+        },
+        2000: {
+          items: 5,
+          gutter: 6
+        }
+      }
+  });
 }
