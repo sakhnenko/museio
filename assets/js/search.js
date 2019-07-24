95ca021064b9b18c9cc0ef1e695c09a4f2de8911
slava <slavas@gmail.com>
2019-07-24 18:09:48 -0300

    Search values to hotjar

--- assets/js/search.js
+++ assets/js/search.js
@@ -6,7 +6,11 @@ if (document.getElementsByClassName("searchable")[0]!=null){
         valueNames: [ 'story_title', 'artPiece', 'artLoc']
       }
     var storyList = new List('stories', options)
-
+    storyList.on("updated", function(list){
+      if (list.searched == true) { 
+        hj('vpv', '/search/story/'+document.getElementsByClassName('search')[0].value); 
+      };
+    })
 
     var options2 = {
         valueNames: [ 'museumLabel' ]
@@ -16,6 +20,7 @@ if (document.getElementsByClassName("searchable")[0]!=null){
     galleryList.on("updated", function(list){
         if (list.searched == true) {
             console.log("Searched")
+            hj('vpv', '/search/gallery/'+document.getElementsByClassName('search')[0].value); 
             document.getElementById("gal_display").style.display = 'none'
             document.getElementById("gal_search").style.display = 'flex '
           } else {
